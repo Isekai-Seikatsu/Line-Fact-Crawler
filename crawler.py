d1892924cddf4855980bcfa55c541011c26b81b6
isekai <brian1689878638@gmail.com>
2019-09-05 06:23:38 +0800

    Fetch until no content, instead of hardcoded pages

--- crawler.py
+++ crawler.py
@@ -3,6 +3,8 @@ import json
 
 import pandas as pd
 
+from itertools import count
+
 
 url = "https://api-fact-checker.line-apps.com/pub/v1/zhtw/articles/verified?size=12&sort=updatedAt,desc&page={}"
 
@@ -15,15 +17,16 @@ def fetch_and_filt(page):
     yield from ([post['id'], post['content'], post['tag']['en'], post['tag']['zhtw']]
                 for post in data['content'])
 
-def fetch_pages(page_range):
-    for page in page_range:
-        yield from fetch_and_filt(page)
+def fetch_pages():
+    for page in count():
+        contents = list(fetch_and_filt(page))
+        if contents:
+            yield from contents
+        else:
+            return
 
 
-pages = 116
-gen = fetch_pages(range(pages))
-
-source = list(gen)
+source = list(fetch_pages())
 
 df = pd.DataFrame(source, columns=['id', 'content', 'tag_en', 'tag_zhtw'])
 
